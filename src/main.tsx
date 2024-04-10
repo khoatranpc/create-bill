3650c1fe6992f3827b7e8bcf34152b2a54ebeee5
khotranpc <khotranpc603@gmail.com>
2024-04-10 20:06:39 +0700

    update route

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Layout from './layout.tsx';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,8 +10,10 @@ import Bill from './components/Bill/index.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='/bill' element={<Bill />} />
+            <Route path='/' element={<Layout />} >
+                <Route path='' element={<App />} />
+                <Route path='/bill' element={<Bill />} />
+            </Route>
         </Routes>
     </BrowserRouter>
 )
