1c0606481a10477da2037d85c19806fe2cbedb56
khotranpc <khotranpc603@gmail.com>
2024-04-10 22:04:53 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Layout from './layout.tsx';
-import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Bill from './components/Bill/index.tsx';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
